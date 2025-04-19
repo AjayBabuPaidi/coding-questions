2258d6ee34ee631c48d19b2cda4dce9c3c22f6fb
AJAY BABU PAIDI <ajaybabupaidi@AJAYs-MacBook-Pro.local>
2025-04-19 06:17:11 +0530

    leetcode 2215

---
+++ leetcode-easy/arrays-and-hashing/2215.py
@@ -0,0 +1,35 @@
+# LeetCode Question 
+    ## 884. Uncommon Words from Two Sentences
+# LeetCode Link
+    ## https://leetcode.com/problems/uncommon-words-from-two-sentences/description/
+# NeetCode Explanation
+    ## https://www.youtube.com/watch?v=24IYW1kQdYU&ab_channel=NeetCodeIO
+
+
+from typing import List
+
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        nums1_not_in_nums2 = set()
+        nums2_not_in_nums1 = set()
+        for each_number in nums1:
+            if each_number not in nums2:
+                nums1_not_in_nums2.add(each_number)
+
+        for each_number in nums2:
+            if each_number not in nums1:
+                nums2_not_in_nums1.add(each_number)
+        return [list(nums1_not_in_nums2), list(nums2_not_in_nums1)]
+
+sol = Solution()
+
+# Example 1
+nums11 = [1,2,3]
+nums21 = [2,4,6]
+print(sol.findDifference(nums11, nums21))
+
+# Example 2
+nums12 =[1,2,3,3]
+nums22 = [1,1,2,2]
+print(sol.findDifference(nums12, nums22))
